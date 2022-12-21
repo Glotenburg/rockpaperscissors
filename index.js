44eb5b7be3cd410f6ddc15805fd380ef7db83987
Glotenburg <albin.hamberg@gmail.com>
2022-12-21 22:27:59 +0100

    add start to second function of a single round

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 function getComputerChoice() {
     let randomNumber = Math.random();
-console.log(randomNumber)
     if (randomNumber < 0.33) {return 'rock';
 }   else if (randomNumber > 0.67) {return 'paper';
 }   else {return 'scissors'
@@ -9,3 +8,14 @@ console.log(randomNumber)
 }
 
 console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    const computerSelection = getComputerChoice();
+    const playerSelection = ('rock', 'paper', 'scissors');
+
+    if (playerselection ='rock' && computerSelection='rock') {return 'tie'
+    } else if (playerselection ='rock' && computerSelection='paper') {return 'you lose, paper beats rock'
+    } else if (playerselection ='rock' && computerSelection='scissors') {return 'you win, rock beats paper'
+}
+
+}
