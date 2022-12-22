c2a7588a15aba94b6185f7889d396bbd03b97fae
Glotenburg <albin.hamberg@gmail.com>
2022-12-22 23:04:06 +0100

    made something wrong but write stuff

--- index.js
+++ index.js
@@ -9,13 +9,23 @@ function getComputerChoice() {
 
 console.log(getComputerChoice());
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
     const computerSelection = getComputerChoice();
     const playerSelection = ('rock', 'paper', 'scissors');
+    return {playerSelection, computerSelection} 
 
-    if (playerselection ='rock' && computerSelection='rock') {return 'tie'
-    } else if (playerselection ='rock' && computerSelection='paper') {return 'you lose, paper beats rock'
-    } else if (playerselection ='rock' && computerSelection='scissors') {return 'you win, rock beats paper'
+           if (playerSelection ='rock' && computerSelection='rock') {return 'tie'
+    } else if (playerSelection ='rock' && computerSelection='paper') {return 'you lose, paper beats rock'
+    } else if (playerSelection ='rock' && computerSelection='scissors') {return 'you win, rock beats paper'
+
+    } else if (playerSelection ='paper' && computerSelection='rock') {return 'You win, paper beats rock'
+    } else if (playerSelection ='paper' && computerSelection='paper') {return 'tie'
+    } else if (playerSelection ='paper' && computerSelection='scissors') {return 'you lose, scissors beats paper'
+
+    } else if (playerSelection ='scissors' && computerSelection='rock') {return 'You lose, rock beats scissors'
+    } else if (playerSelection ='scissors' && computerSelection='paper') {return 'You win, scissors beat paper'
+    } else if (playerSelection ='scissors' && computerSelection='scissors') {return 'tie'
+
 }
-
+console.log(playRound());
 }
